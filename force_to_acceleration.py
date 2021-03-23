a2c41fb75da476cc4542034baaea38d109a93a03
ESuarez2000 <77457853+ESuarez2000@users.noreply.github.com>
2021-03-22 22:19:47 -0400

    Create force_to_acceleration.py

---
+++ force_to_acceleration.py
@@ -0,0 +1,4 @@
+def force_to_acceleration( m, F ):
+    """Function that calculates the acceleration of body with mass m [Kg] when forceF [ N ] is applied on it.Input: m [kg], F [ N ] Output: a [ m/s^2 ]"""
+    a=F/m
+    return a
